b3da2f922b031b1ef6d8a14d38d21876fb1e850d
wadpas <wadpas@gmail.com>
2024-12-25 19:47:42 +0200

    added emoji stickers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
+import React from 'react'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   )
 }
